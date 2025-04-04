d98853392a9dd45529c2382dbe3b8cdf68a58056
John Paul <paulloayon6406@gmail.com>
2025-04-04 15:20:18 -0700

    Implement role-based authorization, forgot password, and CRUD

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('rootpath')();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({origin:(origin, callback)=> callback(null, true), credentials: true}));
 app.use('/accounts', require('./accounts/accounts.controller'));
 app.use('/api-docs', require('_helpers/swagger'));
 
