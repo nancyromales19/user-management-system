9b7ef1d71d76f88ff0c3d4de4c30c2bed1258670
chrisnagurrea12 <chrisnagurrea942@gmail.com>
2025-04-06 04:37:11 +0800

    Continuation of frontend-profile-admin-fake-backend

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,4 +1,4 @@
-import { AccountService } from '@app/_services';
+import { AccountService } from '../_services/account.service';
 
 export function appInitializer(accountService: AccountService) {
     return () => new Promise(resolve => {
