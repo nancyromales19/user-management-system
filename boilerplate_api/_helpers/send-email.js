81a4f88a09503687fe81a4cc242602128bb92d7b
nancyromales19 <nancyromales16@gmail.com>
2025-04-07 14:40:26 +0800

    Updated

---
+++ boilerplate_api/_helpers/send-email.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const config = require('config.json');
+
+module.exports = sendEmail;
+
+
+async function sendEmail({ to, subject, html, from = config.emailFrom }) {
+    const transporter = nodemailer.createTransport(config.smtpOptions);
+    await transporter.sendMail({ from, to, subject, html});
+}
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'charlene17@ethereal.email',
+        pass: 'B2wq9VN7cvuRrbh7ed'
+    }
+});
