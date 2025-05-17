c5191f7cccd782672953c99317b6f0622a6c1f9c
nancyromales19 <120442272+nancyromales19@users.noreply.github.com>
2025-05-18 01:31:15 +0800

    Delete frontend directory

--- frontend/src/app/_models/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-    id?: string;
-    employeeId: string;
-    account: string;
-    position: string;
-    department: string;
-    hireDate: string;
-    status: 'Active' | 'Inactive';
-}
