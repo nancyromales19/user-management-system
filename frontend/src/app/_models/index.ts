ef4748df2be2b7443143bdaf703496d03df421c9
Nancy <nancyromales16@gmail.com>
2025-05-17 23:39:15 +0800

    cleaning

--- frontend/src/app/_models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './account';
-export * from './alert';
-export * from './role';
-export * from './request';
-export * from './employee';
-export * from './department';
-export * from './workflow';
--- frontend/src/app/_services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './account.service';
-export * from './alert.service';
-export * from './request.service';
-export * from './employee.service';
-export * from './department.service';
-export * from './workflow.service';
